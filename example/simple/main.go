ebdada3d3d25f99a436958ea9d9f9102ea08a8cf
kqbi <bikq@163.com>
2019-10-31 18:53:35 +0800

    change route

--- example/simple/main.go
+++ example/simple/main.go
@@ -8,7 +8,7 @@ package main
 import (
 	"log"
 
-	"github.com/kardianos/service"
+	"github.com/kqbi/service"
 )
 
 var logger service.Logger
--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/kardianos/service
+module github.com/kqbi/service
 
 go 1.10
 
